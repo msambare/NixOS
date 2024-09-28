30b5fa369bfe13dca43ed62872780de78b428fa8
Mangesh Sambare <mangesh@sambare.in>
2024-09-28 16:19:25 +0530

    home-manager custom config addition - wip

--- users/sudo-samurai/home-module-config/alacritty.nix
+++ users/sudo-samurai/home-module-config/alacritty.nix
@@ -1,21 +1,25 @@
-{ lib, config, username, ... }: 
-
-let
-  cfg = config.alacritty;
-
-in
-
-  lib.mkIf cfg.enable {
-    programs.alacritty.settings = {
-        font = {
-          size = 12;
-          family = "FiraCode";
-        };
-        colors = {
-          primary = {
-            background = "#1e1e1e";
-            foreground = "#c5c8c6";
+{ pkgs, lib, config, username, ... }: {
+  options = {
+    alacritty.enable = lib.mkEnableOption "enables alacritty";
+  };
+  
+  config = lib.mkIf config.alacritty.enable {
+    home-manager.users.${username} = {
+      programs.alacritty = {
+        enable = true;
+        settings = {
+          font = {
+            size = 12;
+            family = "FiraCode";
+          };
+          colors = {
+            primary = {
+              background = "#1e1e1e";
+              foreground = "#c5c8c6";
+            };
           };
         };
       };
-  }
+    };
+  };
+}
