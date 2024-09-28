3c95c6cfadc7e8c1027f7ad49e8f2a7acd44af3f
Mangesh Sambare <mangesh@sambare.in>
2024-09-28 16:35:51 +0530

    home-manager custom config addition - wip

--- users/sudo-samurai/home-module-config/alacritty.nix
+++ users/sudo-samurai/home-module-config/alacritty.nix
@@ -1,15 +1,17 @@
-programs.alacritty = {
-  enable = true;
-  settings = {
-    font = {
-      size = 12;
-      family = "FiraCode";
-    };
-    colors = {
-      primary = {
-        background = "#1e1e1e";
-        foreground = "#c5c8c6";
+{ pkgs, lib, config, username, ... }: {
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 12;
+        family = "FiraCode";
+      };
+      colors = {
+        primary = {
+          background = "#1e1e1e";
+          foreground = "#c5c8c6";
+        };
       };
     };
   };
-};
+}
