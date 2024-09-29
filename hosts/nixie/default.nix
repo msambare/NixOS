872a172ea574e96a05daa445e503fcb761246d18
Mangesh Sambare <mangesh@sambare.in>
2024-09-29 17:26:04 +0530

    docker

--- hosts/nixie/default.nix
+++ hosts/nixie/default.nix
@@ -133,13 +133,13 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+}
 
+{ config, pkgs, ... }: {
   # Enable Docker in NixOS
   services.docker = {
     enable = true;                     # Enables the Docker service
     package = pkgs.docker_27;          # Specifies Docker version 27 (optional)
     extraOptions = "--experimental";   # Optional: Add any extra options you want Docker to run with
   };
-
 }
-
