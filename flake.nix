0c54300908d0d4f3915f04ddb0d6723562d90592
Mangesh Sambare <mangesh@sambare.in>
2024-09-29 20:03:03 +0530

    isolating nvchad error

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
         containerization = [ "docker-compose" ];
         development = [ "python312" "staruml" "sublime-merge" ];
         disk_tools = [ "fio" "gparted" "hdparm" "impression" "iotop" "smartmontools" "sysstat" "usbimager" "ventoy-full" ];
-        editors = [ "sublime4" "typora" "vimPlugins.nvchad" "vscode-with-extensions" ];
+        editors = [ "sublime4" "typora" "vscode-with-extensions" ];
 
         default_browser = "brave";
 
