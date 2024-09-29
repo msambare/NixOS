a314471705d40d9fb90a7cd33dc916fc840e208e
Mangesh Sambare <mangesh@sambare.in>
2024-09-29 17:04:41 +0530

    trying to get docker to work at a system level

--- modules/containerization/docker_27/default.nix
+++ modules/containerization/docker_27/default.nix
@@ -5,8 +5,9 @@
     };
 
     config = lib.mkIf config.docker_27.enable  {
-        environment.systemPackages = [
-            pkgs.docker_27
-        ];
-    };
+        services.docker = {
+        enable = true;
+        package = pkgs.docker_27;  # You can specify this or omit it to use the default Docker version.
+        extraOptions = "--experimental";  # Optional: Add any extra options you want Docker to run with.
+  };
 }
