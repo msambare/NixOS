9a7cf6d69df1db8f5669caa16a2f007314e8e25c
Mangesh Sambare <mangesh@sambare.in>
2024-09-29 20:58:34 +0530

    isolating nvchad error

--- modules/editors/vimPlugins.nvchad-ui/default.nix
+++ modules/editors/vimPlugins.nvchad-ui/default.nix
@@ -1,10 +1,10 @@
 { pkgs, lib, config, username, ... }: {
 
     options = {
-        vimPlugins.nvchad-ui.enable = lib.mkEnableOption "enables vimPlugins.nvchad-ui";
+        vimPlugins-nvchad-ui.enable = lib.mkEnableOption "enables vimPlugins.nvchad-ui";
     };
 
-    config = lib.mkIf config.vimPlugins.nvchad-ui.enable  {
+    config = lib.mkIf config.vimPlugins-nvchad-ui.enable  {
         home-manager.users.${username} = {pkgs, ... }: {
             home.packages = with pkgs; [ vimPlugins.nvchad-ui ];
         };
