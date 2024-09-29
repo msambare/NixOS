19c56d4a462f9074e0a309318140e7d2b8d69435
Mangesh Sambare <mangesh@sambare.in>
2024-09-29 11:14:18 +0530

    added programs

--- users/sudo-samurai/modules.nix
+++ users/sudo-samurai/modules.nix
@@ -1,13 +1,7 @@
 { pkgs, ... }: {
 
   imports = [
-    ../../modules/api_client
-    ../../modules/archive
-    ../../modules/audio
-    ../../modules/backup
     ../../modules/browser
-    ../../modules/cloud_tool
-    ../../modules/command_line_tool
     ../../modules/terminal
   ];
 
