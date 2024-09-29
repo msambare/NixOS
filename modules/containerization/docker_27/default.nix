783fb70c43781da157a523ee405080ddaad6d8f1
Mangesh Sambare <mangesh@sambare.in>
2024-09-29 16:58:34 +0530

    installing docker as a system service

--- modules/containerization/docker_27/default.nix
+++ modules/containerization/docker_27/default.nix
@@ -5,12 +5,8 @@
     };
 
     config = lib.mkIf config.docker_27.enable  {
-        home-manager.users.${username} = {pkgs, ... }: {
-            home.packages = with pkgs; [ docker_27 
-                shadow  # for newuidmap and newgidmap
-                slirp4netns  # for user-mode networking
-                fuse-overlayfs  # for rootless overlay filesystem support
-            ];
-        };    
+        environment.systemPackages = [
+            pkgs.docker_27
+        ];
     };
 }
