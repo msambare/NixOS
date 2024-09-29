9d7bd156e59aee1d41f6a98315ad7ad5db7506df
Mangesh Sambare <mangesh@sambare.in>
2024-09-29 20:48:54 +0530

    permitted insecure package openssl 1.1

--- hosts/nixie/default.nix
+++ hosts/nixie/default.nix
@@ -119,7 +119,7 @@
   # allow insecure packages - needed for some program in development group
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w"
-  ]
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
