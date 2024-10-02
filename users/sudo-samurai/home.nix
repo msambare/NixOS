33de04b861053ced138aced6085792267eb7622a
Mangesh Sambare <mangesh@sambare.in>
2024-10-02 12:23:14 +0530

    ts

--- users/sudo-samurai/home.nix
+++ users/sudo-samurai/home.nix
@@ -12,13 +12,4 @@
     userEmail = "mangesh@sambare.in";
   };
 
-  #programs.fish.enable = true;
-
-  # Add your custom aliases
-  # programs.fish.aliases = {
-  #   ll = "ls -la";
-  #   gst = "git status";
-  #   .. = "cd ..";
-  # };
-
 }
