f1c36d820bacff176003a5da68eae2c06fc3d879
Mangesh Sambare <mangesh@sambare.in>
2024-10-03 15:06:43 +0530

    ts - making pkgs unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/release-24.05";
