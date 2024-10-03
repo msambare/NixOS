d10a5852ef9f8df90fa8d2e8d1d600772538869f
Mangesh Sambare <mangesh@sambare.in>
2024-10-03 21:19:11 +0530

    ts - trying allowing unfree in flakes.nix and commented allow free in default.nix (configuration.nix)

--- hosts/nixie/default.nix
+++ hosts/nixie/default.nix
@@ -94,8 +94,8 @@
   programs.firefox.enable = true;
 
   # Allow unfree packages
-  pkgs.config.allowUnfree = true;
-  pkgs-unstable.config.allowUnfree = true;
+  # pkgs.config.allowUnfree = true;
+  # pkgs-unstable.config.allowUnfree = true;
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes"];
