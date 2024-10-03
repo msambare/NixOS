4122905b20badb34011786863fcca0d358b9ac37
Mangesh Sambare <mangesh@sambare.in>
2024-10-03 14:05:32 +0530

    ts

--- hosts/nixie/default.nix
+++ hosts/nixie/default.nix
@@ -95,7 +95,6 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  nixpkgs-unstable.config.allowUnfree = true;
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes"];
