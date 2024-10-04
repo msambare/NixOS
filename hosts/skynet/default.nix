4da1b03c48f4068e9c2f76b181053827ce048d3a
msambare <mangesh@sambare.in>
2024-10-04 11:24:47 +0530

    integrated 1password cli as per official docs

--- hosts/skynet/default.nix
+++ hosts/skynet/default.nix
@@ -93,6 +93,17 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # Enables the 1Password CLI
+  programs._1password = { enable = true; };
+
+  # Enables the 1Password desktop app
+  programs._1password-gui = {
+  enable = true;
+  
+  # this makes system auth etc. work properly
+  polkitPolicyOwners = [ "sudo-samurai" ];
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
